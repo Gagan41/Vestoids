68c4191d48e1f81fcc8d7d1ee6e52fc6c04493cb
Gagan41 <gagant.p100@gmail.com>
2024-11-04 21:42:37 +0530

    Frontend Files v0.0.1

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -81,7 +81,7 @@ const Home = () => {
       setIsLoading(true); // Start loading
 
       const response = await axios.post(
-        `${import.meta.env.VITE_API_URL}/contacts`,
+        `${import.meta.env.VITE_API_URL}/api/contacts`,
         formData
     );
 
